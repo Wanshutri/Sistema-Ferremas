21d8ced63957b31a19b6cbc605be2c9feb4b78d0
Wanshutri <valenzuelavivancorfelipe@gmail.com>
2024-04-22 15:35:41 -0400

    fc

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// Importa el módulo Express
+const express = require('express');
+
+// Crea una instancia de la aplicación Express
+const app = express();
+
+// Define una ruta básica que responde con un mensaje
+app.get('/', (req, res) => {
+  res.send('¡Hola desde Express!');
+});
+
+// Configura el servidor para escuchar en el puerto 3001 o el que se le asigne a travez de variables de entorno
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`---------------------------------------------\nServidor Express escuchando en el puerto ${PORT}\n---------------------------------------------`);
+});
