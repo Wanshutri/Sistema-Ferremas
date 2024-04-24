5f5abdac0392d8caa161ce2081f6a1f673a9e4dc
Pipee <103954167+WhoIsPipee@users.noreply.github.com>
2024-04-23 21:11:46 -0400

    NO BORRAR (SOLO SI ESTA MALO)
    Negro no borres mi contenido

--- frontend/src/views/home/home.js
+++ frontend/src/views/home/home.js
@@ -2,12 +2,25 @@ import NavBar from "../../components/navBar/navBar";
 
 const Home = () => {
     return (
+        
         <div>
             <header>
                 <NavBar></NavBar> {/* Podemos importar componentes en otros componentes, este caso importamos el componente navbar en el componente de la propia pagina home */}
+                <div>
+                    <img src="https://6.soompi.io/wp-content/uploads/image/9f3cf588eb694f87a06660b9a72d5d26/dummy.jpeg?s=900x600&e=t" alt="" />
+                </div>
             </header> 
             <body> 
-               <h1>Pene</h1> 
+                <div>
+                    <p>Foto</p>
+                    <p>Barra de navegacion</p>
+                </div>
+                <div>
+                    <p>Slider</p>
+                </div>
+                <div>
+                    <p>Categorias</p>
+                </div>
             </body>
         </div>
     )
