2def4b2589dcdf7b7eed8903b4532f02e7871f0a
XFelipenombregenericoX <fe.valenzuelav@duocuc.cl>
2024-04-29 14:11:04 -0400

    conexion a mysql

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,14 @@ const app = express();
 
 // Define una ruta básica que responde con un mensaje
 app.get('/', (req, res) => {
-  res.send('¡Hola desde Express!');
+    res.send('¡Hola desde Express!');
 });
 
-// Configura el servidor para escuchar en el puerto 3001 o el que se le asigne a travez de variables de entorno
+// Importa la conexión a la base de datos
+const connection = require('./resources/conection');
+
+// Configura el servidor para escuchar en el puerto 3001 o el que se le asigne a través de variables de entorno
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`---------------------------------------------\nServidor Express escuchando en el puerto ${PORT}\n---------------------------------------------`);
+    console.log(`---------------------------------------------\nServidor Express escuchando en el puerto ${PORT}\n---------------------------------------------`);
 });
