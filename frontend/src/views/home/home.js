d9426fdc7fc45f14562c34ca4d779e3f1517b34e
SiegAxel <f.torresa@hotmail.com>
2024-04-29 21:03:33 -0400

    Homepage 90%

--- frontend/src/views/home/home.js
+++ frontend/src/views/home/home.js
@@ -1,11 +1,15 @@
+import './home.css'
 import React from "react";
 import LeftMenu from "../../components/leftMenu/leftMenu";
 import Footer from "../../components/footer/footer";
 import BannerCom from "../../components/banner/bannerCom"
 import logo from "./../../img/logo.png"
 import banner from "./../../img/banner.png"
-import flicking from "../../components/flicking/flicking"
-
+import bob from "./../../img/bob.jpg"
+import sucursales from "./../../img/sucursales.jpg"
+import oferta from "./../../img/oferta.jpg"
+import Carousel from "../../components/carousel/carousel"
+import Cards from "../../components/cards/cards"
 
 
 const Home = () => {
@@ -18,7 +22,31 @@ const Home = () => {
                         <BannerCom />
                     </header>
                     <main>
-                        {/* Main content */}
+                        <div className="search-div d-flex">
+                            <img className="logo-img" src={logo} alt="Logo"></img>
+                            <div className="d-flex form-div">
+                                <button className="btn border-0 shadow-none btnsearch" type="submit">
+                                    <svg fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                                    </svg>
+                                </button>
+                                <input className="" type="search" placeholder="Buscar" aria-label="Search" />
+                            </div>
+                        </div>
+                        <Carousel imagen1={bob} imagen2={sucursales} />
+                        <div class="container text-center">
+                            <div class="row">
+                                <div class="col">
+                                    <Cards imagen={bob} />
+                                </div>
+                                <div class="col">
+                                    <Cards imagen={bob}/>
+                                </div>
+                                <div class="col">
+                                    <Cards imagen={bob} />
+                                </div>
+                            </div>
+                        </div>
 
                     </main>
                     <footer>
@@ -27,7 +55,7 @@ const Home = () => {
                 </div>
             </div>
         </div>
-        
+
     )
 }
 
