5913bfbf91c7bb563f50e119c97874c96e824d00
SiegAxel <f.torresa@hotmail.com>
2024-04-30 13:16:24 -0400

    Commit merge NEgro

--- frontend/src/components/navBar/navBar.js
+++
@@ -1,9 +0,0 @@
-import './navBar.css' // Podemos importar estilos independientes para cada componentes (solo se aplciaran a este y no globalmente)
-
-const NavBar = () => {
-    return ( // SOLO UN NAVBAR DE PRUEBA SACADO DE BOOTSTRAP
-        0
-    )
-}
-
-export default NavBar;
