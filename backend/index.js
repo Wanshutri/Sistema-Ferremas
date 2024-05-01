c2f58e56ebd15f3891251c606eb5031464c93ab6
Wanshutri <valenzuelavivancofelipe@gmail.com>
2024-04-30 20:28:23 -0400

    api lista

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const connection = require('./resources/connection');
+const { getProductos } = require('./resources/DAOproductos');
 
 const app = express();
 
-const con = connection()
-
 app.get('/', (req, res) => {
-    res.send('CONEXION\n' + con);
+    getProductos()
+        .then(productos => {
+            // Cuando la promesa se resuelve con éxito, se ejecuta este bloque de código
+            res.send('Datos obtenidos desde la base de datos: ' + JSON.stringify(productos));
+        })
+        .catch(error => {
+            // Si hay algún error al obtener los productos, se ejecuta este bloque de código
+            console.error('Error al obtener productos:', error);
+            res.status(500).send('Error al obtener productos desde la base de datos');
+        });
 });
 
 const PORT = process.env.PORT || 3001;
--- backend/resources/connection.js
+++ backend/resources/connection.js
@@ -10,40 +10,9 @@ const dbConfig = {
     database: process.env.DB_DATABASE
 };
 
-const result = require('dotenv').config();
-
-if (result.error) {
-    console.error('Error al cargar el archivo .env:', result.error);
-}
-
 // Función para establecer la conexión a la base de datos
 function conectar() {
-    //console.log('Valores de las variables de entorno:');
-    //console.log('DB_HOST:', process.env.DB_HOST);
-    //console.log('DB_PORT:', process.env.DB_PORT);
-    //console.log('DB_USER:', process.env.DB_USER);
-    //console.log('DB_DATABASE:', process.env.DB_DATABASE);
-    //console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-    //console.log('Variables de entorno disponibles:', process.env);
-
     const connection = mysql.createConnection(dbConfig);
-
-    // Intenta conectarte a la base de datos
-    connection.connect((err) => {
-        if (err) {
-            console.error('Error al conectar a la base de datos:', err);
-            return;
-        }
-        console.log('Conexión exitosa a la base de datos MySQL.');
-    });
-
-    // Escuchar eventos de error y cerrar conexión
-    connection.on('error', (err) => {
-        console.error('Error en la conexión a la base de datos:', err);
-        connection.destroy();
-    });
-
-    // Retornar la conexión establecida
     return connection;
 }
 
