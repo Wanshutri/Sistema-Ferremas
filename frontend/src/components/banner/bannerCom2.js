2fe14e4020453ea4d7f16a541ce1a87b991ad632
syrdraken <manuel.miquelesalvarez@gmail.com>
2024-05-02 18:56:13 -0400

    02-05-18:56

---
+++ frontend/src/components/banner/bannerCom2.js
@@ -0,0 +1,12 @@
+import './banner.css';
+import banner from './../../img/banner.png';
+
+
+const BannerCom2 = () => {
+    return (
+        <><img className="banner" src={banner} alt="Banner"></img>
+          </>
+    )
+}
+
+export default BannerCom2;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from './views/home/home';
+import Contactanos from './views/contactanos/contactanos1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +15,7 @@ root.render(
         <Route index element={<Home></Home>} /> {/*Podemos importar paginas y componentes para cada ruta pertinente*/}
         <Route path="test" element={<h1>PAGINA DE TEST</h1>} />
         <Route path="*" element={<h1>Pagina no encontrada</h1>} />
+        <Route path="contactanos" element={<Contactanos></Contactanos>} />
       </Route>
     </Routes>
   </BrowserRouter>
