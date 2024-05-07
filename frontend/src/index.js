2af241e3d0700929e2adac3dcb8846f856455ebd
Pipee <103954167+WhoIsPipee@users.noreply.github.com>
2024-05-07 16:46:54 -0400

    Login/Registro/Recuperar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sobrenosotros from './views/sobrenosotros/sobrenosotros';
 import Ayuda from './views/ayuda/ayuda'
-
 import Home from './views/home/home';
-
+import Login from './views/login/login'
+import Registro from './views/registro/registro';
+import RecuperarContraseña from './views/recuperarContraseña/recuperarContraseña';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
@@ -17,6 +18,9 @@ root.render(
         <Route index element={<Home></Home>} /> {/*Podemos importar paginas y componentes para cada ruta pertinente*/}
         <Route path="test" element={<h1>PAGINA DE TEST</h1>} />
         <Route path="sobrenosotros" element={<Sobrenosotros></Sobrenosotros>} />
+        <Route path="login" element={<Login></Login>} />
+        <Route path="registro" element={<Registro></Registro>} />
+        <Route path="recuperarContraseña" element={<RecuperarContraseña></RecuperarContraseña>} />
         <Route path="ayuda" element={<Ayuda></Ayuda>} />
         <Route path="*" element={<h1>Pagina no encontrada</h1>} />
       </Route>
