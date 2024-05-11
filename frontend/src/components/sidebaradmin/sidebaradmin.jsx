f8238cf80846120de3f689d9290b8323507b8f22
SiegAxel <f.torresa@hotmail.com>
2024-05-10 20:33:08 -0400

    Un monton de weas hice ayuda 10px;

---
+++ frontend/src/components/sidebaradmin/sidebaradmin.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  BiBookAlt,
+  BiHome,
+  BiMessage,
+  BiSolidReport,
+  BiStats,
+  BiTask,
+  BiHelpCircle,
+} from "react-icons/bi";
+import styles from "./sidebaradmin.module.css";
+
+const Sidebaradmin = () => {
+  return (
+    <div className={styles.menu}>
+      <div className={styles.logo}>
+        <BiBookAlt className={styles.logoicon} />
+        <h2>Ferremas</h2>
+      </div>
+
+      <div className={styles.menulist}>
+        <a href="" className={`${styles.item} active`}>
+          <BiHome className={styles.icon} />
+          Dashboard
+        </a>
+        <a href="" className={styles.item}>
+          <BiTask className={styles.icon} />
+          Cosa 2
+        </a>
+        <a href="" className={styles.item}>
+          <BiSolidReport className={styles.icon} />
+          Cosa 3
+        </a>
+        <a href="" className={styles.item}>
+          <BiStats className={styles.icon} />
+          Cosa 4
+        </a>
+        <a href="" className={styles.item}>
+          <BiMessage className={styles.icon} />
+          Cosa 5
+        </a>
+        <a href="" className={styles.item}>
+          <BiHelpCircle className={styles.icon} />
+          Cosa 6
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebaradmin;
